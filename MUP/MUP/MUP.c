336ee26f71ff5dfede244ccbb028bd3847938c2f
wolfik0buka <molodoi_padavan@mail.ru>
2016-03-21 16:49:40 +0300

    New Progect. Nothing in it.

---
+++ MUP/MUP/MUP.c
@@ -0,0 +1,17 @@
+/*
+ * MUP.c
+ *
+ * Created: 21.03.2016 16:46:26
+ *  Author: Egor
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
